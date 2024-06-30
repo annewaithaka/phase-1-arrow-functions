36c9bb80d17a62a857201ddda4084c2d5933674b
annewaithaka <annewaithaka68@gmail.com>
2024-06-30 15:42:07 +0300

    complete lab

---
+++ index.js
@@ -0,0 +1,14 @@
+let divide = (a, b) => {
+    if (b === 0) {
+     new Error('Division by zero is not allowed');
+    }
+    return a / b;
+  };
+
+let square = (x) => {
+    return x * x;
+  };
+
+let add = (a, b) => {
+    return a + b;
+  };
